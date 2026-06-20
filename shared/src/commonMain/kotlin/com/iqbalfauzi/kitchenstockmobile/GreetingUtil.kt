e609214c60489d0f3a7963700aad6ba75506cc1e
Iqbalbbx <iqbal.fauzi@bobobox.com>
2026-06-20 18:28:36 +0700

    Kitchen Stock Mobile

---
+++ shared/src/commonMain/kotlin/com/iqbalfauzi/kitchenstockmobile/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.iqbalfauzi.kitchenstockmobile
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/com/iqbalfauzi/kitchenstockmobile/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.iqbalfauzi.kitchenstockmobile
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
